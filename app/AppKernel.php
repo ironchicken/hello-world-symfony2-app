abef26c0856417fc3c6c73c1a6974a73100d5b10
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2014-10-23 08:44:35 +0200

    Gajdaw: hello world page

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new My\UserBundle\MyUserBundle(),
+            new Gajdaw\HelloWorldBundle\GajdawHelloWorldBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Gajdaw/HelloWorldBundle/GajdawHelloWorldBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Gajdaw\HelloWorldBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GajdawHelloWorldBundle extends Bundle
+{
+}
