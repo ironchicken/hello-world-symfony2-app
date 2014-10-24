dc26bb2bcba99468bdaf2289785e17ed51c8b2cc
PMazur92 <mazur.piotr92@gmail.com>
2014-10-24 09:54:25 +0200

    PMazur: web page hey

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new My\UserBundle\MyUserBundle(),
             new Gajdaw\HelloWorldBundle\GajdawHelloWorldBundle(),
+            new PMazur\HeyBundle\PMazurHeyBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
