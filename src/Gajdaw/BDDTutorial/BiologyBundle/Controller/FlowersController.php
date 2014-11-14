89e850e85c7e34ad30e2899739a1c9b155746440
PMazur92 <mazur.piotr92@gmail.com>
2014-11-14 10:09:52 +0100

    Test file for Biology/Flowers/Rose page 2

---
+++ src/Gajdaw/BDDTutorial/BiologyBundle/Controller/FlowersController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Gajdaw\BDDTutorial\BiologyBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class FlowersController extends Controller
+{
+    /**
+     * @Route("/biology/flowers/rose.html")
+     * @Template()
+     */
+    public function roseAction()
+    {
+        return array();
+    }
+}
