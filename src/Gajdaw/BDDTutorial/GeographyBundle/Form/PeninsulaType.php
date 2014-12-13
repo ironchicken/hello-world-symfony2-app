4909e7dd07bd28c9f5ab5159f4abf7b3c780fce9
Tomasz Warecki-Franczak <tomasz.warecki,franczak@gmail.com>
2014-12-13 10:53:31 +0100

    New item form for Peninsulas

--- src/Gajdaw/BDDTutorial/GeographyBundle/Form/PeninsulaType.php
+++ src/Gajdaw/BDDTutorial/GeographyBundle/Form/PeninsulaType.php
@@ -26,7 +26,11 @@ class PeninsulaType extends AbstractType
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'Gajdaw\BDDTutorial\GeographyBundle\Entity\Peninsula'
+            'data_class' => 'Gajdaw\BDDTutorial\GeographyBundle\Entity\Peninsula',
+            'attr' => array(
+                'class' => 'form-horizontal',
+                'role'  => 'form',
+            ),
         ));
     }
 
