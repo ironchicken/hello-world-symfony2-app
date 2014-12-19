387ed07a0ddb760494449df2d20ea06598d393f8
Tomasz Warecki-Franczak <tomasz.warecki,franczak@gmail.com>
2014-12-19 09:23:43 +0100

    Fixtures for Desert Entity

---
+++ src/Gajdaw/BDDTutorial/GeographyBundle/DataFixtures/ORM/LoadDeserts.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Gajdaw\BDDTutorial\GeographyBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Yaml\Yaml;
+use Gajdaw\BDDTutorial\GeographyBundle\Entity\Desert;
+
+class LoadDeserts implements FixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+
+        $filename = __DIR__ . '/../../data/deserts.yml';
+        $yml = Yaml::parse(file_get_contents($filename));
+        foreach ($yml as $item) {
+            $desert = new Desert();
+            $desert->setName($item['name']);
+            $desert->setArea($item['length']);
+            $manager->persist($desert);
+        }
+
+        $manager->flush();
+    }
+}
